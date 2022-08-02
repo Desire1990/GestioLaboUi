a0b494ce81cbe9763f4cd4664d9f413602986b63
Desire1990 <desnduwim26@gmail.com>
2022-08-02 11:43:49 +0200

    main components

---
+++ src/components/main.vue
@@ -0,0 +1,235 @@
+<template>
+    <div class="recent-grid">
+        <div class="projects">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="heading">Recent Projects</h3>
+                    <button>See all <span class="fas fa-arrow-right"></span></button>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table width="100%">
+                            <thead>
+                                <tr>
+                                    <td>Project Title</td>
+                                    <td>Department</td>
+                                    <td>Status</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>UI/UX Design</td>
+                                    <td>UI Team</td>
+                                    <td>
+                                        <span class="status purple"></span>review
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Web Dev</td>
+                                    <td>Frontend</td>
+                                    <td>
+                                        <span class="status pink"></span>pending
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Ushop App</td>
+                                    <td>Mobile Team</td>
+                                    <td>
+                                        <span class="status orange"></span>active
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>UI/UX Design</td>
+                                    <td>UI Team</td>
+                                    <td>
+                                        <span class="status purple"></span>review
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Web Dev</td>
+                                    <td>Frontend</td>
+                                    <td>
+                                        <span class="status pink"></span>pending
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Ushop App</td>
+                                    <td>Mobile Team</td>
+                                    <td>
+                                        <span class="status orange"></span>active
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>UI/UX Design</td>
+                                    <td>UI Team</td>
+                                    <td>
+                                        <span class="status purple"></span>review
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Web Dev</td>
+                                    <td>Frontend</td>
+                                    <td>
+                                        <span class="status pink"></span>pending
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Ushop App</td>
+                                    <td>Mobile Team</td>
+                                    <td>
+                                        <span class="status orange"></span>active
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>UI/UX Design</td>
+                                    <td>UI Team</td>
+                                    <td>
+                                        <span class="status purple"></span>review
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Web Dev</td>
+                                    <td>Frontend</td>
+                                    <td>
+                                        <span class="status pink"></span>pending
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>Ushop App</td>
+                                    <td>Mobile Team</td>
+                                    <td>
+                                        <span class="status orange"></span>active
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="customers">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="heading">New customer</h3>
+                    <button>Sell all<span class="fas fa-arrow-right"></span></button>
+                </div>
+
+                <div class="card-body">
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>
+                                    lion Emusky</h4>
+                                <small>CEO @ TLS</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <span class="fas fa-user-circle"></span>
+                            <span class="fas fa-comment"></span>
+                            <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>Ion Emusky</h4>
+                                <small>C0O @ AFK</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                        <span class="fas fa-user-circle"></span>
+                        <span class="fas fa-comment"></span>
+                        <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>El-lion musky</h4>
+                                <small>CEO @ FOMO</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                        <span class="fas fa-user-circle"></span>
+                        <span class="fas fa-comment"></span>
+                        <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>John Bezos</h4>
+                                <small>CEO @ Mazon</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <span class="fas fa-user-circle"></span>
+                            <span class="fas fa-comment"></span>
+                            <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>Beelz Gai8</h4>
+                                <small>CEO @ Mycroft</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <span class="fas fa-user-circle"></span>
+                            <span class="fas fa-comment"></span>
+                            <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>Jeam Krook</h4>
+                                <small>CEO @ Penapple</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <span class="fas fa-user-circle"></span>
+                            <span class="fas fa-comment"></span>
+                            <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                    <div class="customer">
+                        <div class="info">
+                            <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                                alt="">
+                            <div>
+                                <h4>Cris Coya</h4>
+                                <small>CEO @ PenCode</small>
+                            </div>
+                        </div>
+                        <div class="contact">
+                            <span class="fas fa-user-circle"></span>
+                            <span class="fas fa-comment"></span>
+                            <span class="fas fa-phone"></span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
