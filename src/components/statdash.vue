6bbc320c9eab1f6fe680f18aad8af12b20ed209d
Desire1990 <desnduwim26@gmail.com>
2022-08-02 11:44:49 +0200

    top bare on dashhboard

---
+++ src/components/statdash.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="main-wrapper">
+    <div class="main-content">
+    	<main>
+    		<Stat/>
+    	    <Main/>
+    		<Footer/>
+    	</main>
+        </div>
+    </div>
+
+</template>
+<script>
+	import Main from "../components/main.vue"
+	import Footer from "../components/footer.vue"
+	import Stat from "../components/stat.vue"
+	export default{
+		components:{Main, Footer, Stat},
+	}
+</script>
---
+++ src/components/topbar.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="mai-wrapper">
+		<div class="main-content">
+			            <header>
+                <h2 class="heading" id="dashboard">
+                    Dashboard
+                    </h2>
+                    <label for="nav-toggle">
+                        <span class="fas fa-bars"></span>
+                    </label>
+                  
+                <div class="search">
+                    <div class="search-rotate">
+                    <div class="icon"></div>
+                    </div>
+                    <div class="input">
+                        <input type="text" placeholder="search" id="mysearch" autocomplete="off" onkeydown="display(this)">
+                       
+                    </div>
+                    
+                </div>
+               
+
+
+
+                <div class="user-wrapper">
+                    <img src="https://assets.codepen.io/3853433/internal/avatars/users/default.png?format=auto&version=1617122449&width=40&height=40"
+                        alt="">
+                    <div>
+                        <h4>Memoire</h4>
+                        <small><b>2022</b></small>
+                    </div>
+                </div>
+            </header>
+		</div>
+	</div>
+</template>
