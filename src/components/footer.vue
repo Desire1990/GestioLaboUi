1a0b3899f161b2d228e820fb4fb8edd913fcb5ac
Desire1990 <desnduwim26@gmail.com>
2022-08-02 11:43:34 +0200

    footer page

---
+++ src/components/footer.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="footer">
+        <div class="word">
+        <center>
+            <p>Made with <span id="hrt"><i class="far fa-heart"></i></span> | Memoire 2022</p>        	
+        </center>    
+        </div>
+    </div>
+</template>
