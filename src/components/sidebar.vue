edc32fd17526051cfea88be3ddca9a610f0a540d
Desire1990 <desnduwim26@gmail.com>
2022-08-02 11:44:22 +0200

    sidebar and stat component in actions

---
+++ src/components/sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+	 <input type="checkbox" id="nav-toggle">
+    <!-- side bar    -->
+    <div class="sidebar">
+            <div class="sidebar-brand">
+                <h2><span><i class="fab fa-accusoft"></i></span><span id="kleenpulse">Gestion Lab</span></h2>
+            </div>
+            <div class="sidebar-menu">
+                <ul>
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active" ><span class="fas fa-users"></span><strong>Dashboard</strong></router-link>
+                    </li>
+
+                    <li>
+                     <router-link to="/about" class="nav-item nav-link active"><span class="fas fa-users"></span><strong>Customers</strong></router-link>
+                    </li>
+                    
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active"><span class="fas fa-clipboard-list"></span><strong>Projects</strong></router-link>
+                    </li>
+                    
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active"><span class="fas fa-shopping-bag"></span>
+                            <strong>Orders</strong></router-link>
+                    </li>
+                    
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active"><span class="fas fa-receipt"></span><strong>Inventory</strong></router-link>
+                    </li>
+                    
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active"><span class="fa fa-user-circle"></span><strong>Accounts</strong></router-link>
+                    </li>
+                    <li>
+                     <router-link to="/" class="nav-item nav-link active"><span class="fas fa-clipboard"></span><strong>Task</strong></router-link>
+                    </li>
+                </ul>
+            </div>
+    </div>
+    <!-- side -->
+	
+</template>
+<script>
+	export default{
+		data(){
+			return{
+
+			}
+		},
+	}
+</script>
---
+++ src/components/stat.vue
@@ -0,0 +1,98 @@
+<template>
+    <div id="pop-wrap">
+        <h1 class="pop-up">Light Mode Activated</h1>
+    </div>
+    <div class="switch" id="switch">
+        <div id="toggle" @click='toggled'>
+            <i class="fas fa-moon"></i>
+            <i class="fas fa-sun"></i>
+            <i class="indicator"></i>
+
+        </div>
+    </div>
+    <div class="cards">
+        <div class="card-single">
+            <div>
+                <h1 id="customer"></h1>
+                <span>Happy Clients</span>
+            </div>
+            <div>
+                <span class="fas fa-users"></span>
+            </div>
+        </div>
+        <div class="card-single">
+            <div>
+                <h1 id="project"></h1>
+                <span>Projects Done!</span>
+            </div>
+            <div>
+                <span class="fas fa-clipboard"></span>
+            </div>
+        </div>
+        <div class="card-single">
+            <div>
+                <h1 id="order"></h1>
+                <span>Confirmed Orders</span>
+            </div>
+            <div>
+                <span class="fas fa-shopping-bag"></span>
+            </div>
+        </div>
+        <div class="card-single">
+            <div>
+                <h1><sup>$</sup>
+                    <p id="income"><b>k</b></p>
+                </h1>
+                <span>weekly Income</span>
+            </div>
+            <div>
+                <span class="fab fa-google-wallet"></span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+	import Main from "../components/main.vue"
+	import Footer from "../components/footer.vue"
+	export default{
+		components:{Main, Footer},
+		data(){
+			return{
+
+			}
+		},
+		mounted(){
+			if(media.matches){
+					console.log(true)
+					$('#dashboard').mouseenter(function(){
+					this.innerHTML = `good
+				    ${welcome}`;
+					});
+					$('#dashboard').mouseleave(function(){
+						this.innerHTML = "DASHBOARD";
+					});
+					$('#kleenpulse').mouseenter(function(){
+						this.innerHTML = "welcome";
+					});
+					$('#kleenpulse').mouseleave(function(){
+						this.innerHTML = "LiquidTime";
+					});
+					}else{
+						console.log(false)
+					}
+
+		},
+		methods:{
+			toggled(){
+				const body = document.querySelector('body');
+				const toggles = document.getElementById('toggle');
+				const media = window.matchMedia("(min-width:700px)")
+				body.classList.toggle('light');
+				toggles.classList.toggle('active')
+			}
+		}
+			
+
+
+	}
+</script>
