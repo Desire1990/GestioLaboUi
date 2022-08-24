9276c7ee123adbf3bedcaa2cd7e6c8f01a3af3a0
Desire1990 <desnduwim26@gmail.com>
2022-08-24 12:29:19 +0200

    main configured with font

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// import { library, dom } from "@fortawesome/fontawesome-svg-core";
-// import { fas } from '@fortawesome/free-solid-svg-icons'
-// import { fab } from '@fortawesome/free-brands-svg-icons';
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
 // import { far } from '@fortawesome/free-regular-svg-icons';
-// library.add(fas, far, fab)
-// dom.watch();
+library.add(fas, fab)
+dom.watch();
 import "bootstrap/dist/css/bootstrap.min.css"
 import "font-awesome/css/font-awesome.min.css"
 // import '@/main.css'
