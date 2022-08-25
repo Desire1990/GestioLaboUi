b9f9b9479a0f6c1857a07e5e02e4ec304a5360f9
Desire1990 <desnduwim26@gmail.com>
2022-08-25 17:39:48 +0200

    domain pages integrated

---
+++ src/views/Domain.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="main-wrapper">
+      <div class="main-content">
+      <main>
+        <div class="about">
+          <domain/>
+        </div>
+        <top/>
+        <side/>
+        <Footer/>
+      </main>
+    </div>
+  </div>
+</template>
+<script>
+  import Main from "../components/main.vue"
+  import Footer from "../components/footer.vue"
+  import side from '@/components/sidebar.vue'
+  import top from '@/components/topbar.vue'
+  import Stat from "../components/stat.vue"
+  import domain from "../components/domains.vue"
+  export default{ 
+    components:{Main, Footer, Stat, top,side, domain},
+    mounted() {
+        document.title = 'Dashboard | Gestion Labo'
+    },
+  }
+</script>
+<style>
+   
+</style>
